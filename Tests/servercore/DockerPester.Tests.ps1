5a000abc5978252ea9d57279e7ac8f37e85a3242
Kevin Bates <kevin@Kevins-MacBook-Pro-2.local>
2020-03-03 18:31:04 +0100

    updated tests

--- Tests/servercore/DockerPester.Tests.ps1
+++ Tests/servercore/DockerPester.Tests.ps1
@@ -2,7 +2,7 @@ Import-Module Pester
 Import-Module "$PSScriptRoot/../../DockerPester/DockerPester.psd1" -Force
 
 Describe "Testing Base Funcionality" {
-    $res = Invoke-DockerPester -Image "mcr.microsoft.com/powershell:7.0.0-rc.3-windowsservercore-1803-kb4537762-amd64" -InputFolder "$PSScriptRoot/../TestModule/" -Executor WIN -PathToTests "Tests" -PathOnContainer "c:/Temp"
+    $res = Invoke-DockerPester -Image "mcr.microsoft.com/windows/servercore:ltsc2019" -InputFolder "$PSScriptRoot/../TestModule/" -Executor WIN -PathToTests "Tests" -PathOnContainer "c:/Temp"
     it "Result should contain PassThru Object from Pester" {
         $res.TestResult | Should -Not -BeNullOrEmpty
     }
